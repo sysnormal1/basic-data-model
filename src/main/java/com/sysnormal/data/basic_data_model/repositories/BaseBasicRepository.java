327f03a06f3e6cba9baf689cc28521c22d705cea
ti-jumbo <jumbo.ti@jumboalimentos.com.br>
2026-03-25 18:43:28 -0300

    adjusted versions

--- src/main/java/com/sysnormal/data/basic_data_model/repositories/BaseBasicRepository.java
+++ src/main/java/com/sysnormal/data/basic_data_model/repositories/BaseBasicRepository.java
@@ -2,7 +2,9 @@ package com.sysnormal.data.basic_data_model.repositories;
 
 import com.sysnormal.data.base_data_model.repositories.BaseCommonRepository;
 import com.sysnormal.data.basic_data_model.entities.BaseBasicEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -13,7 +15,5 @@ public interface BaseBasicRepository<E extends BaseBasicEntity<E>, ID> extends B
     Optional<E> findByDataOriginIdAndIdAtOrigin(Long dataOriginId, String idAtOrigin);
 
     Optional<E> findByDataOriginIdAndTableOriginIdAndIdAtOrigin(Long dataOriginId, Long tableOriginId, String idAtOrigin);
-
-
 
 }
--- src/main/java/com/sysnormal/data/basic_data_model/repositories/database/TablesRepository.java
+++ src/main/java/com/sysnormal/data/basic_data_model/repositories/database/TablesRepository.java
@@ -1,23 +1,27 @@
 package com.sysnormal.data.basic_data_model.repositories.database;
 
+import com.sysnormal.data.basic_data_model.entities.database.tables.Tables;
 import com.sysnormal.data.basic_data_model.entities.records.dataOrigin.DataOrigin;
 import com.sysnormal.data.basic_data_model.repositories.BaseBasicRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
-public interface TablesRepository extends BaseBasicRepository<DataOrigin, Long> {
+public interface TablesRepository extends BaseBasicRepository<Tables, Long> {
     @Query(value = """
-    select t.id
-    from `tables` t
-    join `schemas` s on s.id = t.schema_id and s.name = :schemaName
-    where t.name = :name
-""", nativeQuery = true)
-    Optional<Long> customGetIdBySchemaNameAndName(
-            @Param("schemaName") String schemaName,
-            @Param("name") String name
+        select t.id
+        from `tables` t
+        join `schemas` s on s.id = t.schema_id and s.name = :schemaName
+        where t.name = :name
+    """, nativeQuery = true)
+    Optional<Long> getIdBySchemaNameAndName(
+        @Param("schemaName") String schemaName,
+        @Param("name") String name
     );
+
 }
